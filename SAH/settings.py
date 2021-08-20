20ca5fea47cd8793ad067d335d83b63341dc3190
lalit21-logico <59661128+lalit21-logico@users.noreply.github.com>
2021-08-20 11:05:11 +0530

    Update settings.py

--- SAH/settings.py
+++ SAH/settings.py
@@ -84,8 +84,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lytt10025@gmail.com'
-EMAIL_HOST_PASSWORD = 'ly21mvrmvr'
+EMAIL_HOST_USER = 'EMAIL HERE'
+EMAIL_HOST_PASSWORD = 'PASS HERE'
 
 WSGI_APPLICATION = 'SAH.wsgi.application'
 
